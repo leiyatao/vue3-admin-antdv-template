7f051009d012ae2097ca41c9cef36ef7a6b0cf31
leiyatao <leiyatao0204@qq.com>
2024-10-14 17:40:29 +0800

    antdv组件库全局按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import { StepBackwardOutlined,StepForwardOutlined,HomeOutlined } from "@ant-design/icons-vue";
+</script>
 <template>
   <div>Hello vue3</div>
-  <a-button type="primary">Add</a-button>
+  <a-button type="primary"><HomeOutlined/>主色按钮</a-button>
+  <StepBackwardOutlined />
+  <StepForwardOutlined />
 </template>
 
 <style scoped></style>
